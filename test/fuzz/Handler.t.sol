2dddcc5e99fc72480ebe97c422df21a31a113328
Komron Khisomov <normok_9595@bk.ru>
2024-04-03 01:37:07 +0500

    Handler/redeemCollateral

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -13,15 +13,16 @@ import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
-
-contract Handler is Test{
+contract Handler is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
 
     ERC20Mock weth;
     ERC20Mock wbtc;
-    
-    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc){
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
         dsce = _dscEngine;
         dsc = _dsc;
 
@@ -32,11 +33,27 @@ contract Handler is Test{
 
     function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
         ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
         dsce.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
     }
 
-    function _getCollateralFromSeed(uint256 collateralSeed) private view returns(ERC20Mock) {
-        if(collateralSeed % 2 == 0){
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateralToRedeem = dsce.getCollateralBalanceOfUser(address(collateral), msg.sender);
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if (amountCollateral == 0){
+            return;
+        }
+        dsce.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
             return weth;
         }
         return wbtc;
